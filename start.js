53b78e1f71d926d85c9b524e1c98101897a11968
Devan McCarthy <devan.mccarthy4000@gmail.com>
2022-12-06 20:26:23 -0500

    start.js dynamic test

--- start.js
+++ start.js
@@ -6,7 +6,21 @@ app.use(express.static('css'))
 app.use(express.json())
 
 app.get('/', (req,res) => {
-    res.status(200).json({info: 'present text XD'})
+    const top = '<!DOCTYPE html>' + '\n'  + '<html lang="en">' + '\n' +  '<head>' +   '<meta charset="UTF-8">' + '\n' +       '<meta http-equiv="X-UA-Compatible" content="IE=edge">' + '\n' +         '<meta name="viewport" content="width=device-width, initial-scale=1.0">' + '\n' +         '<title>Document</title>' + '\n' +     '</head>' + '\n' +     '<body>' + '\n';
+    const bottom = '</body>' + '\n' +  '</html>';
+    var stgout = top;
+    //database calls to get the number of jobs in a given day for i < (number of jobs)
+    for (let i = 0; i < 5; i++) {
+    //methods for getting address and time go here for a specific job in a day using the i counter to select the jobs
+    var infoone = 'address'
+    var infotwo = 'time'
+    
+    stgout = stgout.concat('', infoone + ' ' + infotwo + '<br>');
+    
+    
+    }
+    stgout = stgout.concat('',bottom);
+    res.status(200).send(stgout)
 }) 
 
 app.post('/', (req, res) => {
