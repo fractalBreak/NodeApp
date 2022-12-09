42aa36d365221c93a8f879a048c9ba87cfe1c3c0
Devan McCarthy <devan.mccarthy4000@gmail.com>
2022-12-09 18:15:38 -0500

    dynamic webpage 2

--- css/start.js
+++ css/start.js
@@ -5,13 +5,13 @@ const port = 3000;
 
 
 
-export var webapp = app.get('/', (req,res) => {
+webapp = app.get('/', (req,res) => {
     const top = '<!DOCTYPE html>' + '<br>'  + '<html lang="en">' + '<br>' +  '<head>' +   '<meta charset="UTF-8">' + '<br>' +       '<meta http-equiv="X-UA-Compatible" content="IE=edge">' + '<br>' +         '<meta name="viewport" content="width=device-width, initial-scale=1.0">' + '<br>' +         '<title>Document</title>' + '<br>' +     '</head>' + '<br>' +     '<body>' + '<br>';
     const bottom = '</body>' + '<br>' +  '</html>';
     var stgout = top;
     //database calls to get the number of jobs in a given day for i < (number of jobs)
     
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 5; i++) {
     //methods for getting address and time go here for a specific job in a day using the i counter to select the jobs
     var infoone = 'address';
     var infotwo = 'time';
